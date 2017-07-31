016975a783e2d23e3e9bf8b3a729ecf25fe12c27
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2017-07-31 21:20:40 +0300

    1st commit. Initializing repository

---
+++ MicrophoneApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MicrophoneApp
+//
+//  Created by Manolescu Mihai Alexandru on 31/07/2017.
+//  Copyright © 2017 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
