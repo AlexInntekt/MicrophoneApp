88c5b4f5e0c49492fa2fb97ffb25ac5d8a711d47
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2017-08-02 21:03:27 +0300

    the app is currently functional but needs have some comments, and be organized

---
+++ MicrophoneApp/RecorderVC.swift
@@ -0,0 +1,137 @@
+//
+//  RecorderVC.swift
+//  MicrophoneApp
+//
+//  Created by Manolescu Mihai Alexandru on 01/08/2017.
+//  Copyright © 2017 Manolescu Mihai Alexandru. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+import CoreData
+
+
+class RecorderVC: UIViewController, UITextFieldDelegate
+{
+    
+    //OUTLETS SECTION:
+    @IBOutlet weak var recordButton: UIButton!
+    @IBAction func recordButton(_ sender: Any)
+    {
+        if audioRecorder!.isRecording
+          {
+            //stop the recording
+            audioRecorder?.stop()
+            
+            //change the button's title to 'record'
+            recordButton.setTitle("Record", for: .normal)
+            playButton.isEnabled = true
+          }
+        else
+        {
+            //start recording
+            audioRecorder?.record()
+            addButton.isEnabled = true
+            
+            //change button's title to 'stop'
+            recordButton.setTitle("Stop", for: .normal)
+        }
+    }
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBAction func playButton(_ sender: Any)
+    {
+        do {
+         try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+             audioPlayer!.play()
+        } catch {}
+    }
+    
+    
+    @IBOutlet weak var nameTextfield: UITextField!
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBAction func addButton(_ sender: Any)
+    {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let sound = Sound(context: context)
+        sound.name = nameTextfield.text
+        sound.audio = NSData(contentsOf: audioURL!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+        
+        
+    }
+    //END OF OUTLETS SECTION
+    
+    
+    //GLOBAL VARIABLES SECTION:
+    var audioRecorder : AVAudioRecorder? = nil
+    var audioPlayer : AVAudioPlayer?
+    var audioURL : URL?
+    //END OF GLOBAL VARIABLES SECTION
+
+    
+    //dissmis the keyboard after tapping on 'return' from the textField:
+    func textFieldShouldReturn(_ titleTextfield: UITextField) -> Bool
+    {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        nameTextfield.delegate = self
+        
+        setupRecorder()
+        playButton.isEnabled = false
+        addButton.isEnabled = false
+        print("\n\n Audio recording file URL adress: ", audioRecorder?.url ,"\n\n")
+        
+        
+    }
+    
+    func setupRecorder()
+    {
+        do {
+        //create an audio session
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try session.overrideOutputAudioPort(.speaker)
+        try session.setActive(true)
+        
+        
+        //create URL for the audio file
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+                audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            
+        // create settings for the audio recorder
+            var settings : [String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject
+            settings[AVSampleRateKey] = 44100.0 as AnyObject
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject
+            
+        
+        // Create Audiorecorder Object
+        audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
+        audioRecorder!.prepareToRecord()
+        } catch let error as NSError {
+             print("! Error coded by developer with 24g45h4qh. Default details: " , error)
+        }
+            
+    }
+
+
+
+
+}
--- MicrophoneApp/ViewController.swift
+++ MicrophoneApp/ViewController.swift
@@ -7,18 +7,90 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var soundsList : [Sound] = []
+    var audioPlayer : AVAudioPlayer?
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do
+        {
+            soundsList = try context.fetch(Sound.fetchRequest())
+            tableView.reloadData()
+        } catch {}
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return soundsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell()
+        
+        let sound = soundsList[indexPath.row]
+        
+        cell.textLabel?.text = sound.name
+        
+        
+        return cell;
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let sound = soundsList[indexPath.row]
+        let audio = AVAudioPlayer()
+        
+        do
+        {
+        audioPlayer = try AVAudioPlayer(data: sound.audio as! Data)
+        audioPlayer?.play()
+        } catch {}
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+            print("\n Deleting cell \n")
+            let sound = soundsList[indexPath.row]
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            context.delete(sound)
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do
+            {
+                soundsList = try context.fetch(Sound.fetchRequest())
+                tableView.reloadData()
+            } catch {}
+        }
+    }
+
 
 
 }
